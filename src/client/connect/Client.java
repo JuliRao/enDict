738961e1e98c85333deed4acfb74d299980147cf
ddzhouxm@16e.com <ddzhouxm@163.com>
2016-11-29 12:27:00 +0800

    cs

--- src/client/connect/Client.java
+++ src/client/connect/Client.java
@@ -9,11 +9,15 @@ import common.ThreeMeanings;
 
 public class Client {
 	private Socket socket;
+	DataOutputStream toServer;
+	ObjectInputStream input;
 	
 	Client() {
 		try {
 			socket = new Socket("localhost", 3000);
 			System.out.println("server connected.");
+			toServer = new DataOutputStream(socket.getOutputStream());
+			input = new ObjectInputStream(socket.getInputStream());
 			//socket = new Socket("114.212.130.243", 3000);
 		} catch (IOException e) {
 			// TODO 自动生成的 catch 块
@@ -24,11 +28,13 @@ public class Client {
 	public ThreeMeanings searchWord(String word) {
 		ThreeMeanings meanings = null;
 		try {
-			DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
-			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+//			DataOutputStream toServer = new DataOutputStream(socket.getOutputStream());
+//			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
 			
 			toServer.writeUTF(word);
 			meanings = (ThreeMeanings)input.readObject();
+			toServer.flush();
+//			toServer.close();
 		} catch (IOException | ClassNotFoundException e) {
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();

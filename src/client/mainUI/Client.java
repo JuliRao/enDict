c90e86702f439fbe0a379e852139d0c6ad5eebc2
Ma Rao <141220077@smail.nju.edu.cn>
2016-12-11 11:50:04 +0800

    c

--- src/client/mainUI/Client.java
+++ src/client/mainUI/Client.java
@@ -1,37 +1,47 @@
 package client.mainUI;
 
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import common.Dictionary;
 import common.ThreeMeanings;
 import client.common.Info;
 import client.common.SearchableApater;
 import client.common.Send;
+import client.mainUI.functionUI.FunctionButton;
 import client.mainUI.functionUI.FunctionPanel;
 import client.mainUI.functionUI.FunctionPanelCreator;
 import client.theme.MyTheme;
 
 public class Client extends JFrame implements Send {
 	private Socket socket;
-	private DataOutputStream toServer;
-	private ObjectInputStream input;
+	DataOutputStream toServer;
+	ObjectInputStream input;
+	
+	private ImageIcon background = new ImageIcon(MyTheme.Instance().getBackgroundPicture()); 	// 背景图片
+	private ImageIcon background2 = new ImageIcon("data/image/tree.jpg"); 	// 背景图片
+	private JLabel label = new JLabel(background);
 	
 	private MainPane mainPane = new MainPane();
 	private FunctionPanel functionPanel = new FunctionPanelCreator().createFunctionPanel();
-	private ImageIcon background = new ImageIcon(MyTheme.Instance().getBackgroundPicture()); 	// 背景图片
-	private JLabel label = new JLabel(background);
-
-	public Client() {
+	
+	public Client(Socket socket) {
+		this.socket = socket;
+		System.out.println("Server connected.");
+		try {
+			input = new ObjectInputStream(socket.getInputStream());
+			toServer = new DataOutputStream(socket.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		this.setTitle("萌娆词典 - Zhou XinMeng & MaRao");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(740, 600);
@@ -39,76 +49,28 @@ public class Client extends JFrame implements Send {
 		this.setResizable(false);
 		this.setVisible(true);
 		
-		// 添加组件
-		mainPane.setBounds(40, 40, 650, 450);
-        this.add(mainPane);
-        
         functionPanel.setBounds(40, 500, 650, 37);
         this.add(functionPanel);
-		
+        mainPane.setBounds(40, 40, 650, 450);
+        this.add(mainPane);
+        
         // 把背景图片显示在一个标签里面  
         label.setBounds(0, 0, this.getWidth(), this.getHeight());  
         JPanel imagePanel = (JPanel) this.getContentPane();  
         imagePanel.setOpaque(false);  
         this.getContentPane().add(label, new Integer(Integer.MIN_VALUE)); 
         
-        // 关闭窗口时关闭连接
-        this.addWindowListener(new WindowListener() {
-			
-			@Override
-			public void windowOpened(WindowEvent e) {
-				connect();
-			}
-			
-			@Override
-			public void windowIconified(WindowEvent e) {
-
-			}
-			
-			@Override
-			public void windowDeiconified(WindowEvent e) {
-
-			}
-			
-			@Override
-			public void windowDeactivated(WindowEvent e) {
-
-			}
-			
-			@Override
-			public void windowClosing(WindowEvent e) {
-				
-			}
-			
-			@Override
-			public void windowClosed(WindowEvent e) {
-				closeConnection();
-			}
-			
-			@Override
-			public void windowActivated(WindowEvent e) {
-
-			}
-		});
-        
-        
+        // 设置相应操作
+       mainPane.momentsPanel.setSend(this);
+       mainPane.wordPanel.setSend(this);
+        ((FunctionButton) functionPanel.getComponent(1)).setSend(this);
+        ((FunctionButton) functionPanel.getComponent(2)).setSend(this);
 	}
-	
-	public void connect() {
-		try {
-			socket = new Socket("localhost", 8000);
-			System.out.println("Server connected.");
-			toServer = new DataOutputStream(socket.getOutputStream());
-			input = new ObjectInputStream(socket.getInputStream());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
+
 	public void changeBackground() {
 		this.remove(label);
 		
-		label = new JLabel(background);
+		label = new JLabel(background2);
 		label.setBounds(0, 0, this.getWidth(), this.getHeight());  
         JPanel imagePanel = (JPanel) this.getContentPane();  
         imagePanel.setOpaque(false);  
@@ -121,11 +83,13 @@ public class Client extends JFrame implements Send {
 	
 	public void searchWord(String word) {
 		try {
+			Info.setWord(word);
 			toServer.writeUTF(word);
+			toServer.flush();
+
 			ThreeMeanings meanings = (ThreeMeanings)input.readObject();
 			Info.setMeanings(new SearchableApater(meanings));
-			Info.setWord(word);
-			toServer.flush();
+			
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -140,43 +104,41 @@ public class Client extends JFrame implements Send {
 		}
 	}
 	
-	public static void main(String[] args) {
-		new Client();
-	}
-
-	@Override
-	public void getUserList() {
-		// TODO 自动生成的方法存根
-		
-	}
-
-	@Override
-	public void getCards() {
-		// TODO 自动生成的方法存根
-		
-	}
-
-	@Override
 	public void login(String user, String password) {
-		// TODO 自动生成的方法存根
-		
+		System.out.println("Click login");
 	}
-
-	@Override
+	
 	public void signIn(String user, String password) {
-		// TODO 自动生成的方法存根
-		
+		System.out.println("Click sign in");
 	}
-
-	@Override
+	
 	public void sendCard() {
-		// TODO 自动生成的方法存根
+		System.out.println("Start sending...");
+	}
+
+	public void like(Dictionary dictionary) {
+		System.out.println("Like " + dictionary.getName());
+	}
+	
+	public void getUserList() {
+		System.out.println("Get all the user");
+	}
+	
+	public void getCard() {
 		
 	}
 
-	@Override
-	public void like(Dictionary dictionary) {
-		// TODO 自动生成的方法存根
+	public void getCards() {
 		
+	}
+	
+	public static void main(String[] args) {
+		try {
+			new Client(new Socket("localhost", 8000));
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
